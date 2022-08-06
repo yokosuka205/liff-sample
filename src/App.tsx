5c7c8fc8e2339797c4963224e180e04f5e14f039
yokosuka205 <yokosuka205k9@gmail.com>
2022-08-06 15:53:03 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import liff from "@line/liff"; // 追加
 
 function App() {
+  /* 追加: メッセージ送信 */
+  const sendMessage = () => {
+    liff
+      .init({ liffId: process.env.REACT_APP_LIFF_ID as string }) // LIFF IDをセットする
+      .then(() => {
+        if (!liff.isLoggedIn()) {
+          liff.login({}); // ログインしていなければ最初にログインする
+        } else if (liff.isInClient()) {
+          // LIFFので動いているのであれば
+          liff
+            .sendMessages([
+              {
+                // メッセージを送信する
+                type: "text",
+                text: "You've successfully sent a message! Hooray!",
+              },
+            ])
+            .then(function () {
+              window.alert("Message sent");
+            })
+            .catch(function (error) {
+              window.alert("Error sending message: " + error);
+            });
+        }
+      });
+  };
+
+  /* 追加: UserProfileをAlertで表示 */
+  const getUserInfo = () => {
+    liff.init({ liffId: process.env.REACT_APP_LIFF_ID as string }).then(() => {
+      if (!liff.isLoggedIn()) {
+        liff.login({}); // ログインしていなければ最初にログインする
+      } else if (liff.isInClient()) {
+        liff
+          .getProfile() // ユーザ情報を取得する
+          .then((profile) => {
+            const userId: string = profile.userId;
+            const displayName: string = profile.displayName;
+            alert(`Name: ${displayName}, userId: ${userId}`);
+          })
+          .catch(function (error) {
+            window.alert("Error sending message: " + error);
+          });
+      }
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +58,14 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <button className="button" onClick={sendMessage}>
+          send message
+        </button>
+        {/* 追加 */}
+        <button className="button" onClick={getUserInfo}>
+          show user info
+        </button>
+        {/* 追加 */}
         <a
           className="App-link"
           href="https://reactjs.org"
